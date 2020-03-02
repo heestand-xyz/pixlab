e5bc6060aa9ff36ed1a9e1703f9c5ace13167d63
Hexagons <anton@hexagons.se>
2020-03-02 18:53:10 +0100

    color

--- Sources/pixlab/main.swift
+++ Sources/pixlab/main.swift
@@ -77,6 +77,8 @@ struct Pixlab: ParsableCommand {
 //        case imageNotFound
         case corruptImage
         case pixInitFail(String)
+        case unknownArg(Any)
+        case badArg(String)
     }
     
     func run() throws {
@@ -115,6 +117,40 @@ struct Pixlab: ParsableCommand {
     
     func make(_ code: String) throws -> PIX & NODEOut {
         var symbols: [AnyExpression.Symbol: AnyExpression.SymbolEvaluator] = [:]
+        symbols[.function("rgb", arity: .exactly(3))] = { args in
+            guard let vals: [Double] = args as? [Double] else {
+                throw PIXLabError.badArg("arg type is array of floats")
+            }
+            return LiveColor(r: LiveFloat(vals[0]),
+                             g: LiveFloat(vals[1]),
+                             b: LiveFloat(vals[2]))
+        }
+        symbols[.function("rgba", arity: .exactly(4))] = { args in
+            guard let vals: [Double] = args as? [Double] else {
+                throw PIXLabError.badArg("arg type is array of floats")
+            }
+            return LiveColor(r: LiveFloat(vals[0]),
+                             g: LiveFloat(vals[1]),
+                             b: LiveFloat(vals[2]),
+                             a: LiveFloat(vals[3]))
+        }
+        symbols[.function("hsv", arity: .exactly(3))] = { args in
+            guard let vals: [Double] = args as? [Double] else {
+                throw PIXLabError.badArg("arg type is array of floats")
+            }
+            return LiveColor(h: LiveFloat(vals[0]),
+                             s: LiveFloat(vals[1]),
+                             v: LiveFloat(vals[2]))
+        }
+        symbols[.function("hsva", arity: .exactly(4))] = { args in
+            guard let vals: [Double] = args as? [Double] else {
+                throw PIXLabError.badArg("arg type is array of floats")
+            }
+            return LiveColor(h: LiveFloat(vals[0]),
+                             s: LiveFloat(vals[1]),
+                             v: LiveFloat(vals[2]),
+                             a: LiveFloat(vals[3]))
+        }
         for auto in AutoPIXGenerator.allCases {
             let name: String = auto.rawValue.replacingOccurrences(of: "pix", with: "")
             symbols[.function(name, arity: .exactly(1))] = { args in
@@ -143,21 +179,53 @@ struct Pixlab: ParsableCommand {
             symbols[.infix(infix)] = { args in
                 let blendPix = BlendPIX()
                 blendPix.blendMode = blendMode
-                blendPix.inputA = self.argToPix(args[0])
-                blendPix.inputB = self.argToPix(args[1])
+                blendPix.placement = .aspectFill
+                blendPix.extend = .hold
+                blendPix.inputA = try self.argToPix(args[0])
+                blendPix.inputB = try self.argToPix(args[1])
                 return blendPix
             }
         }
         let expression = AnyExpression(code, constants: PIXs.pixs, symbols: symbols)
-        let pix: PIX & NODEOut = try expression.evaluate()
+        let pix: PIX & NODEOut = try argToPix(try expression.evaluate())
         return pix
     }
     
-    func argToPix(_ arg: Any) -> PIX & NODEOut {
+    func argToPix(_ arg: Any) throws -> PIX & NODEOut {
         if let pix = arg as? PIX & NODEOut {
             return pix
         }
-//        else if let val = arg as? 
+        if let color: LiveColor = argToColor(arg) {
+            let colorPix = ColorPIX(at: .square(1))
+            colorPix.color = color
+            return colorPix
+        }
+        throw PIXLabError.unknownArg(arg)
+    }
+    
+    func argToColor(_ arg: Any) -> LiveColor? {
+        if let color: LiveColor = arg as? LiveColor {
+            return color
+        }
+        var color: LiveColor?
+        if let val = arg as? Double {
+            color = LiveColor(lum: LiveFloat(val))
+        } else if let vals = arg as? [Double] {
+            if vals.count == 2 {
+                color = LiveColor(lum: LiveFloat(vals[0]),
+                                  a: LiveFloat(vals[1]))
+            } else if vals.count == 3 {
+                color = LiveColor(r: LiveFloat(vals[0]),
+                                  g: LiveFloat(vals[1]),
+                                  b: LiveFloat(vals[2]))
+            } else if vals.count == 4 {
+                color = LiveColor(r: LiveFloat(vals[0]),
+                                  g: LiveFloat(vals[1]),
+                                  b: LiveFloat(vals[2]),
+                                  a: LiveFloat(vals[3]))
+            }
+        }
+        return color
     }
     
     func assign(_ command: String) throws -> Bool {
